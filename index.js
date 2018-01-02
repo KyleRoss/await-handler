63f1d12eb9b089604c9bee5cb3ad967a5bdbd48c
Kyle Ross <kylerross1324@gmail.com>
2018-01-02 13:01:56 -0500

    Add ability to pass properties to the error object

--- index.js
+++ index.js
@@ -1,12 +1,14 @@
 "use strict";
 /**
  * Handler which takes a promise and returns array signature with `[err, data]`.
- * @param  {Promise} promise Promise to handle
- * @return {Array}           Array with signature `[err, data]`
+ * @param  {Promise} promise    Promise to handle
+ * @param  {Object}  errorProps Object containing additional properties to include in a returned error
+ * @return {Array}              Array with signature `[err, data]`
  */
-module.exports = function awaitHandler(promise) {
+module.exports = function awaitHandler(promise, errorProps) {
     return promise.then(data => {
         return [null, data];
-    })
-    .catch(err => [err]);
+    }).catch(err => {
+        if(errorProps) Object.assign(err, errorProps);
+    });
 };
