5c9e99eea36026981bad95f452399ecff46a58e1
Kyle Ross <kylerross1324@gmail.com>
2018-07-06 10:59:06 -0400

    Update jsdoc

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 "use strict";
 /**
- * Handler which takes a promise and returns array signature with `[err, data]`.
+ * Handler which takes a promise and resolves with array signature `[err, results]`.
  * @param  {Promise} promise    Promise to handle
  * @param  {Object}  errorProps Object containing additional properties to include in a returned error
- * @return {Array}              Array with signature `[err, data]`
+ * @return {Promise<Array>}     Array with signature `[err, results]`
  */
 module.exports = function awaitHandler(promise, errorProps) {
     return promise.then(data => {
